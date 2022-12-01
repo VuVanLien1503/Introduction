fc33d0e2b8d43794e10fae5f7dab8c0198625edd
VuVanLien1503 <vanlien.vu1503@gmail.com>
2022-12-01 15:47:28 +0700

    Tính Số Ngày Trong Tháng

---
+++ src/NgayTrongThang.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class NgayTrongThang {
+    public static void main(String[] args) {
+        int result;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhap Thang Can Xem");
+        int month=scanner.nextInt();
+        if (month>0&&month<=12){
+            switch (month){
+                case 2:
+                    System.out.println("Tháng "+month+"Có 28 or 29 Ngày");
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    System.out.println("Tháng "+month+" Có 31 Ngày");
+                    break;
+                default:{
+                    System.out.println("thang "+ month +" Co 30 ngay");
+                }
+            }
+        }else {
+            System.out.println("Thang Khong Dung");
+        }
+
+    }
+}
